845825ce469a497d62244266a97ac7184d1eacb6
Denis Osotov <desotovs@gmail.com>
2020-10-16 22:31:03 +0300

    add search

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,6 +1,5 @@
-var domWatchList = $("#section_watchlist ul");
-var domMovieList = $("#section_browse ul");
-
+var watchList = $("#section_watchlist ul");
+var browseList = $("#section_browse ul");
 var model = {
   watchListItems: [],
   browseItems: [],
@@ -10,7 +9,15 @@ var api = {
   token: myApiKey,
 };
 
-/* Makes an AJAX request to themoviedb.org, asking for some movies if successful, updates the model.browseItems appropriately, and then invokes the callback function that was passed in */
+$(document).ready(function () {
+  discoverMovies(render);
+});
+
+$("#movie_search").submit((event) => {
+  event.preventDefault();
+  searchMovies($("#search_input").val(), render);
+});
+
 function discoverMovies(callback) {
   $.ajax({
     url: `${api.root}/discover/movie`,
@@ -18,73 +25,62 @@ function discoverMovies(callback) {
       api_key: api.token,
       page: randInt(1, 100),
     },
-    success: function (response) {
+    success: (response) => {
       model.browseItems = response.results;
       callback();
     },
   });
 }
 
-console.log("The script loaded!");
-// re-renders the page with new content, based on the current state of the model
-function render() {
-  domWatchList.empty();
-  domMovieList.empty();
-  model.watchListItems.forEach((movie) => {
-    let liMovie = $("<li>", { class: "movie_item" });
-    let movieTitle = $("<p>", {
-      html: `${movie.title}`,
-      class: "movie_title",
-    });
-    let movieOverview = $("<p>", {
-      html: `${movie.overview}`,
-      class: "movie_overview",
-    });
-    liMovie.append(movieTitle, movieOverview);
-    domWatchList.append(liMovie, $("<div>", { class: "item_del" }));
-  });
-
-  model.browseItems.forEach((movie) => {
-    let liMovie = $("<li>", { class: "movie_item" });
-    let movieTitle = $("<p>", {
-      html: `${movie.title}`,
-      class: "movie_title",
-    });
-    let movieOverview = $("<p>", {
-      html: `${movie.overview}`,
-      class: "movie_overview",
-    });
-    liMovie.append(movieTitle, movieOverview);
-    let added = model.watchListItems.filter(
-      (movie) => movie.title == movieTitle.html()
-    ).length;
-    if (!added) {
-      liMovie.append(
-        $("<div>", {
-          html: "Add to Watch List",
-          class: "add_to_WL_btn",
-        }).click(() => {
-          model.watchListItems.push({
-            title: movieTitle.html(),
-            overview: movieOverview.html(),
-          });
-          render();
-        })
-      );
-    } else {
-      liMovie.append(
-        $("<div>", {
-          html: "Aready in your Watch List!",
-          class: "already_in_WL",
-        })
-      );
-    }
-    domMovieList.append(liMovie, $("<div>", { class: "item_del" }));
+function searchMovies(searchFilm, callback) {
+  $.ajax({
+    url: `${api.root}/search/movie`,
+    data: {
+      api_key: api.token,
+      query: searchFilm,
+    },
+    success: (response) => {
+      model.browseItems = response.results;
+      callback();
+    },
   });
 }
 
-// When the HTML document is ready, we call the discoverMovies function,
-// and pass the render function as its callback
-$(document).ready(function () {
-  discoverMovies(render);
-});
+function movieBlockCreate(movie) {
+  let movieBlock = $("<li>").addClass("movie_block");
+  let title = $("<p>").text(movie.title).addClass("movie_title");
+  let overview = $("<p>").text(movie.overview).addClass("movie_overview");
+  return movieBlock.append(title, overview);
+}
+
+function render() {
+  watchList.empty();
+  browseList.empty();
+  model.watchListItems.forEach((movie) => {
+    let movieBlock = movieBlockCreate(movie);
+    watchList.append(movieBlock, $("<div>").addClass("item_del"));
+  });
+  model.browseItems.forEach((movie) => {
+    let movieBlock = movieBlockCreate(movie);
+    let added = model.watchListItems.filter(
+      (movieInList) => movieInList.title == movie.title
+    ).length;
+    if (!added) {
+      let addBtn = $("<div>")
+        .text("Add to Watch List")
+        .addClass("add_to_WL_btn")
+        .click(() => {
+          model.watchListItems.push({
+            title: movie.title,
+            overview: movie.overview,
+          });
+          render();
+        });
+      movieBlock.append(addBtn);
+    } else
+      movieBlock.append(
+        $("<div>").text("Aready in your Watch List!").addClass("already_in_WL")
+      );
+    browseList.append(movieBlock, $("<div>").addClass("item_del"));
+  });
+}
